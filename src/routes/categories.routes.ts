1704ea85e97c6b3137c43cf57e0c1857b2799d50
Gabriel Santo <gvgesanto2@gmail.com>
2022-01-27 14:29:41 -0300

    created a list categories use case and a list categories controller

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRouter = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRouter.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
 });
 
 categoriesRouter.get("/", (req, res) => {
-  const allCategories = categoriesRepository.list();
-
-  return res.json(allCategories);
+  return listCategoriesController.handle(req, res);
 });
 
 export { categoriesRouter };

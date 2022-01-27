03ba1d3ce5bfb088b2630cfab1b15784314a2d23
Gabriel Santo <gvgesanto2@gmail.com>
2022-01-27 14:47:49 -0300

    created the createSpecificationController and refactored the SpecificationsRepository class to use the singleton pattern

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,22 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationUseCase } from "../modules/cars/useCases/createSpecification/CreateSpecificationUseCase";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRouter = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
 specificationsRouter.post("/", (req, res) => {
-  const { name, description } = req.body;
-
-  const createSpecificationUseCase = new CreateSpecificationUseCase(
-    specificationsRepository
-  );
-
-  createSpecificationUseCase.execute({ name, description });
-
-  return res.status(201).send();
+  return createSpecificationController.handle(req, res);
 });
 
 export { specificationsRouter };
